d04d21db91625e91b7500fc9f5f7ef37f83ea4ea
Omar <oa196145@gmail.com>
2025-02-14 23:28:24 +0200

    sign up V2

--- frontend/frontend/src/pages/Signup/SignUp.tsx
+++ frontend/frontend/src/pages/Signup/SignUp.tsx
@@ -4,7 +4,7 @@ import styles from "../../assets/styles/SignUp.module.css";
 const SignUp = () => {
   return (
     <div className={styles.signUp}>
-      {/* Left Side (Coffee Image) */}
+
       <div className={styles.leftSection}>
         <div className={styles.coffeeText}>
           <p>وجبتك بلقطة، صحتك بخيارك</p>
@@ -16,7 +16,7 @@ const SignUp = () => {
         </div>
       </div>
 
-      {/* Right Side (Form Section) */}
+
       <div className={styles.rightSection}>
         <div className={styles.header}>
           <h2 className={styles.title}>إنشاء حساب</h2>
@@ -25,19 +25,19 @@ const SignUp = () => {
           </p>
         </div>
 
-        {/* Form */}
+
         <form className={styles.form}>
-          {/* First Name & Last Name (Aligned Properly) */}
+
 <div className={styles.row}>
-    {/* First Name - Right Side */}
+
 <div className={styles.row}>
-    {/* Last Name - Left Side */}
+
     <div className={styles.lastNameContainer}>
         <label className={styles.label}>الإسم الأخير</label>
         <input type="text" placeholder="الإسم الأخير" className={styles.lastNameInput} />
     </div>
 
-    {/* First Name - Right Side */}
+
     <div className={styles.firstNameContainer}>
         <label className={styles.label}>الإسم الأول</label>
         <input type="text" placeholder="الإسم الأول" className={styles.firstNameInput} />
@@ -45,19 +45,18 @@ const SignUp = () => {
 </div>
 </div>
 
-          {/* Email Field */}
+
           <div className={styles.inputContainer}>
             <label className={styles.label}>البريد الإلكتروني</label>
             <input type="email" placeholder="البريد الإلكتروني" className={styles.inputField} />
           </div>
 
-          {/* Password Field */}
+
           <div className={styles.inputContainer}>
             <label className={styles.label}>كلمة السر</label>
             <input type="password" placeholder="كلمة السر" className={styles.inputField} />
           </div>
 
-          {/* Checkbox */}
           <div className={styles.checkboxContainer}>
             <input type="checkbox" id="terms" className={styles.checkbox} />
             <label htmlFor="terms" className={styles.shroot}>أوافق على الشروط والأحكام</label>
