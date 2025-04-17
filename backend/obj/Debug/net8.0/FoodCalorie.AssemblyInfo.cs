ba462768549cf7d4b3bd46343f33db071ce90563
Amr Khaled <amrkaled222@gmail.com>
2025-04-17 18:04:53 +0200

    kk

--- backend/obj/Debug/net8.0/FoodCalorie.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/FoodCalorie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baecddda997d33a7d09f6c3cf9870eb1a866b3c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
