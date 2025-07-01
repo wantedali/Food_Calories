075b9eb76d24fcdfb9924eac865c9128dc3b60ad
OmarSaid35 <131901268+OmarSaid35@users.noreply.github.com>
2025-07-01 18:07:55 +0300

    usless

--- backend/obj/Debug/net8.0/FoodCalorie.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/FoodCalorie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c883acbc9572657e9b12c501e66376752196fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0593dace04436c256c65a6297ab2254ec6872f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodCalorie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
